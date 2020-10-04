3d5b03dd250914a7b6b208eb292c61704385998a
Betina Borisova <b.borisowa17@gmail.com>
2020-10-04 18:16:52 +0300

    Added simple GUI

--- main.py
+++ main.py
@@ -1,13 +1,53 @@
-#!/usr/bin/python3
+import tkinter as tk
+from tkinter import font
+import tkinter.messagebox as tkmsg
+import math
 
-print("Calculate Compound Interest \n")
+def CalculateCompoundInterest(n, p, i):
+    ci = p * (math.pow((1 + i / 100), n) - 1)
+    tkmsg.showinfo('Compound Interest', round(ci, 2))
 
-n = int(input("Years (n): "))
+app = tk.Tk()
+app.title('Compound Interest Calculator')
+app.geometry('450x300')
 
-p = int(input("Principal (P): "))
+calculator = tk.Frame(app)
+font_style_label = font.Font(family = 'Times', size = 15)
+font_style_entry = font.Font(family = 'Times', size = 13)
 
-i = int(input("Interest in furute, percentage (i): "))
+n_label = tk.Label(calculator, text = 'Years: ', font = font_style_label)
+n_label.grid(row = 0, pady = 10)
 
-ci = p * (pow((1 + i / 100), n) - 1)
+n = tk.Entry(calculator, justify = 'center', font = font_style_entry)
+n.grid(row = 0, column = 1, pady = 10)
 
-print("Compound interest: {0}".format(round(ci, 2)))
+principal_label = tk.Label(calculator, text = 'Principal: ', font = font_style_label)
+principal_label.grid(row = 1, pady = 10)
+
+principal = tk.Entry(calculator, justify = 'center', font = font_style_entry)
+principal.grid(row = 1, column = 1, pady = 10)
+
+interest_label = tk.Label(calculator, text = 'Interest: % ', font = font_style_label)
+interest_label.grid(row = 2, pady = 10)
+
+interest = tk.Entry(calculator, justify = 'center', font = font_style_entry)
+interest.grid(row = 2, column = 1, pady = 10)
+
+calculator.place(anchor = 'c', relx = 0.5, rely = 0.4)
+
+calculate_button = tk.Button(app, text = 'Calculate', command = lambda: CalculateCompoundInterest(int(n.get()), int(principal.get()), int(interest.get())))
+calculate_button.place(anchor = 'c', relx = 0.5, rely = 0.7)
+
+app.mainloop()
+
+# print("Calculate Compound Interest \n")
+#
+# n = int(input("Years (n): "))
+#
+# p = int(input("Principal (P): "))
+#
+# i = int(input("Interest in furute, percentage (i): "))
+#
+# ci = p * (math.pow((1 + i / 100), n) - 1)
+#
+# print("Compound interest: {0}".format(round(ci, 2)))
