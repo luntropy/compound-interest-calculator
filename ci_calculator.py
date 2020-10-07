48594c8af0d5e75302ae75105ab8fb74deba5ebd
Betina Borisova <b.borisowa17@gmail.com>
2020-10-07 09:32:02 +0300

    Refined the code, added final interface design

--- ci_calculator.py
+++ ci_calculator.py
@@ -3,60 +3,69 @@ from tkinter import font
 
 class MainApplication:
     def __init__(self, master):
+        # App configuration
+        # Setup the color theme of the application
+        self.color_theme = {
+            "app_background_color" : "#333333",
+            "box_background_color" : "#404040",
+            "btn_background_color" : "#404040",
+            "highlight_background" : "#000000",
+            "active_background" : "#595959",
+            "text_color" : "#ffffff",
+
+            # Setup the fonts
+            "font_style_label" : font.Font(family = 'Times', size = 17),
+            "font_style_entry" : font.Font(family = 'Times', size = 15)
+        }
+
         self.master = master
-        self.master.title('Compound Interest Calculator')
-        self.master.geometry('450x350')
+        self.master.title("Compound Interest Calculator")
+        self.master.geometry("500x370")
         self.master.resizable(False, False)
+        self.master.configure(bg = self.color_theme["app_background_color"])
 
-        # Setup the color of the application
-        self.app_background_color = '#204060'
-        self.btn_background_color = '#ffffff'
-        self.master.configure(bg = self.app_background_color)
-
-        # Setup the color of the text
-        self.text_color = '#ffffff'
-
-        # Setup fonts
-        self.font_style_label = font.Font(family = 'Times', size = 15)
-        self.font_style_entry = font.Font(family = 'Times', size = 13)
-
-        self.ci_calculator_frame = tk.Frame(self.master, bg = self.app_background_color)
-        self.create_cic_frame(self.ci_calculator_frame, self.font_style_label, self.font_style_entry)
+        # Create the main calculator frame
+        self.ci_calculator_frame = tk.Frame(self.master, bg = self.color_theme["app_background_color"])
+        self.create_cic_frame(self.ci_calculator_frame, self.color_theme)
 
     # Frames - functions
-    def create_cic_frame(self, frame, font_label, font_entry):
-        principal_label = tk.Label(frame, text = 'Principal: ', font = font_label, bg = self.app_background_color, fg = self.text_color)
-        principal_label.grid(row = 0, pady = 10)
-
-        principal = tk.Entry(frame, justify = 'center', font = font_entry)
-        principal.grid(row = 0, column = 1, pady = 10)
-
-        period_label = tk.Label(frame, text = 'Years: ', font = font_label, bg = self.app_background_color, fg = self.text_color)
-        period_label.grid(row = 1, pady = 10)
-
-        period = tk.Entry(frame, justify = 'center', font = font_entry)
-        period.grid(row = 1, column = 1, pady = 10)
-
-        interest_label = tk.Label(frame, text = 'Interest: ', font = font_label, bg = self.app_background_color, fg = self.text_color)
-        interest_label.grid(row = 2, pady = 10)
-
-        interest = tk.Entry(frame, justify = 'center', font = font_entry)
-        interest.grid(row = 2, column = 1, pady = 10)
-
+    def create_cic_frame(self, frame, color_theme):
         # Text box
-        display_result = tk.Text(frame, height = 2, font = font_entry)
-        display_result.grid(row = 3, columnspan = 5, pady = 10)
+        display_result = tk.Text(frame, state = "disabled", height = 3, font = color_theme["font_style_label"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        display_result.grid(row = 0, columnspan = 5)
 
-        calculate_button = tk.Button(frame, text = 'Calculate', bg = self.btn_background_color)
-        calculate_button.grid(row = 4, columnspan = 2, pady = 10)
+        # Principal
+        principal_label = tk.Label(frame, text = "Principal: ", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        principal_label.grid(row = 1, pady = (40, 15))
 
-        frame.pack(expand = True)
+        principal = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        principal.grid(row = 1, column = 1, pady = (40, 15))
+
+        # Period
+        period_label = tk.Label(frame, text = "Years: ", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        period_label.grid(row = 2, pady = 15)
+
+        period = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        period.grid(row = 2, column = 1, pady = 15)
+
+        # Interest
+        interest_label = tk.Label(frame, text = "Interest: ", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        interest_label.grid(row = 3, pady = 15)
+
+        interest = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        interest.grid(row = 3, column = 1, pady = 15)
+
+        # "Calculate" button
+        calculate_button = tk.Button(frame, text = "Calculate", font = color_theme["font_style_entry"], bg = color_theme["btn_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"], activebackground = color_theme["active_background"])
+        calculate_button.grid(row = 4, columnspan = 2, pady = (30, 0))
+
+        frame.pack(expand = False)
 
     # Other functions
     def calculate_ci(p, n, i):
-        print('TO DO')
+        print("TO DO")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = tk.Tk()
     MainApplication(app)
     app.mainloop()
