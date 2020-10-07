96759361d8eda54ff832f8a533612f98a89e15f5
Betina Borisova <b.borisowa17@gmail.com>
2020-10-07 17:09:07 +0300

    Finished version of the calculator

--- main.py
+++ main.py
@@ -1,53 +1,142 @@
-# import tkinter as tk
-# from tkinter import font
-import tkinter.messagebox as tkmsg
+import tkinter as tk
+from tkinter import font
+
 import math
+from decimal import Decimal
 
-def CalculateCompoundInterest(n, p, i):
-    ci = p * (math.pow((1 + i / 100), n) - 1)
-    tkmsg.showinfo('Compound Interest', round(ci, 2))
+class MainApplication:
+    def __init__(self, master):
+        # App configuration
+        # Setup the color theme of the application
+        self.color_theme = {
+            "app_background_color" : "#333333",
+            "box_background_color" : "#404040",
+            "btn_background_color" : "#404040",
+            "error_background_color": "#b33c00",
+            "highlight_background" : "#000000",
+            "active_background" : "#595959",
+            "text_color" : "#ffffff",
 
-# app = tk.Tk()
-# app.title('Compound Interest Calculator')
-# app.geometry('450x300')
+            # Setup the fonts
+            "font_style_label" : font.Font(family = 'Times', size = 17),
+            "font_style_entry" : font.Font(family = 'Times', size = 15)
+        }
 
-# calculator = tk.Frame(app)
-# font_style_label = font.Font(family = 'Times', size = 15)
-# font_style_entry = font.Font(family = 'Times', size = 13)
+        self.master = master
+        self.master.title("Compound Interest Calculator")
+        self.master.geometry("500x370")
+        self.master.resizable(False, False)
+        self.master.configure(bg = self.color_theme["app_background_color"])
 
-# n_label = tk.Label(calculator, text = 'Years: ', font = font_style_label)
-# n_label.grid(row = 0, pady = 10)
+        # Create the main calculator frame
+        self.ci_calculator_frame = tk.Frame(self.master, bg = self.color_theme["app_background_color"])
+        self.create_cic_frame(self.ci_calculator_frame, self.color_theme)
 
-# n = tk.Entry(calculator, justify = 'center', font = font_style_entry)
-# n.grid(row = 0, column = 1, pady = 10)
+    # Frames - functions
+    def create_cic_frame(self, frame, color_theme):
+        # Text box
+        display_result = tk.Text(frame, state = "disabled", height = 3, font = color_theme["font_style_label"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        display_result.grid(row = 0, columnspan = 5)
 
-# principal_label = tk.Label(calculator, text = 'Principal: ', font = font_style_label)
-# principal_label.grid(row = 1, pady = 10)
+        # Principal
+        principal_label = tk.Label(frame, text = "Principal: ", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        principal_label.grid(row = 1, pady = (40, 15))
 
-# principal = tk.Entry(calculator, justify = 'center', font = font_style_entry)
-# principal.grid(row = 1, column = 1, pady = 10)
+        principal = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        principal.grid(row = 1, column = 1, pady = (40, 15))
 
-# interest_label = tk.Label(calculator, text = 'Interest: % ', font = font_style_label)
-# interest_label.grid(row = 2, pady = 10)
+        # Period
+        period_label = tk.Label(frame, text = "Years: ", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        period_label.grid(row = 2, pady = 15)
 
-# interest = tk.Entry(calculator, justify = 'center', font = font_style_entry)
-# interest.grid(row = 2, column = 1, pady = 10)
+        period = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        period.grid(row = 2, column = 1, pady = 15)
 
-# calculator.place(anchor = 'c', relx = 0.5, rely = 0.4)
+        # Interest
+        interest_label = tk.Label(frame, text = "Interest: %", font = color_theme["font_style_label"], bg = color_theme["app_background_color"], fg = color_theme["text_color"])
+        interest_label.grid(row = 3, pady = 15)
 
-calculate_button = tk.Button(app, text = 'Calculate', command = lambda: CalculateCompoundInterest(int(n.get()), int(principal.get()), int(interest.get())))
-# calculate_button.place(anchor = 'c', relx = 0.5, rely = 0.7)
+        interest = tk.Entry(frame, justify = "center", font = color_theme["font_style_entry"], bg = color_theme["box_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"])
+        interest.grid(row = 3, column = 1, pady = 15)
 
-# app.mainloop()
+        # "Calculate" button
+        calculate_button = tk.Button(frame, text = "Calculate", font = color_theme["font_style_entry"], bg = color_theme["btn_background_color"], fg = color_theme["text_color"], borderwidth = 0, highlightbackground = color_theme["highlight_background"], activebackground = color_theme["active_background"], command = lambda: MainApplication.calculate_ci(display_result, color_theme, principal, period, interest))
+        calculate_button.grid(row = 4, columnspan = 2, pady = (30, 0))
 
-# print("Calculate Compound Interest \n")
-#
-# n = int(input("Years (n): "))
-#
-# p = int(input("Principal (P): "))
-#
-# i = int(input("Interest in furute, percentage (i): "))
-#
-# ci = p * (math.pow((1 + i / 100), n) - 1)
-#
-# print("Compound interest: {0}".format(round(ci, 2)))
+        frame.pack(expand = False)
+
+    # Function to calculate the compound interest
+    def calculate_ci(textbox, color_theme, principal, period, interest):
+        try:
+            error_occured = False
+            principal.config(highlightbackground = color_theme["highlight_background"])
+            period.config(highlightbackground = color_theme["highlight_background"])
+            interest.config(highlightbackground = color_theme["highlight_background"])
+
+            p = tk.StringVar()
+            p = principal.get()
+
+            if not (MainApplication.is_float(p) and p):
+                principal.config(highlightbackground = color_theme["error_background_color"])
+                error_occured = True
+            else:
+                p = float(p)
+
+            n = tk.StringVar()
+            n = period.get()
+
+            if not (MainApplication.is_integer(n) and n):
+                period.config(highlightbackground = color_theme["error_background_color"])
+                error_occured = True
+            else:
+                n = int(n)
+
+            i = tk.StringVar()
+            i = interest.get()
+
+            if not (MainApplication.is_float(i) and i):
+                interest.config(highlightbackground = color_theme["error_background_color"])
+                error_occured = True
+            else:
+                i = float(i)
+
+            if error_occured:
+                raise ValueError
+
+            ci = p * (math.pow(1.0 + i / 100.0, n) - 1.0)
+            ci = round(ci, 2)
+            ci_str_result = "Compound interest: " + str(ci) + '\n'
+
+            total_amount = p + ci
+            total_amount_str_result = "Total amount: " + str(total_amount)
+
+            result_message = ci_str_result + total_amount_str_result
+
+            MainApplication.insert_textbox_message(textbox, result_message)
+        except Exception as e:
+            MainApplication.insert_textbox_message(textbox, "\nERROR: Invalid input")
+
+    def insert_textbox_message(textbox, msg):
+        textbox.config(state = "normal")
+        textbox.delete("1.0", tk.END)
+        textbox.insert("1.0", msg)
+        textbox.config(state = "disabled")
+
+    def is_float(x):
+        try:
+            float(x)
+            return True
+        except:
+            return False
+
+    def is_integer(x):
+        try:
+            int(x)
+            return True
+        except:
+            return False
+
+if __name__ == "__main__":
+    app = tk.Tk()
+    MainApplication(app)
+    app.mainloop()
--- master.py
+++
@@ -1,6 +0,0 @@
-import ci_calculator
-
-if __name__ == '__main__':
-    app = tk.Tk()
-    MainApplication(app)
-    app.mainloop()
